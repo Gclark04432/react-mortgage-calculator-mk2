aa131b597744c82da64c85449760acab751ca5c8
Gary Clark <gclark04432@live.co.uk>
2020-03-04 12:00:38 +0000

    can update state of firstSalary on change

--- src/components/MortgageForm.js
+++ src/components/MortgageForm.js
@@ -5,9 +5,16 @@ class MortgageForm extends Component {
   constructor(props){
     super(props);
     this.state={
-      applicants: 1
+      applicants: 1,
+      criteria: {
+        firstSalary: 0,
+        secondSalary: 0,
+        deposit: 0,
+        otherCommitments: 0
+      }
     }
     this.handleApplicantsChange = this.handleApplicantsChange.bind(this);
+    this.handlefirstSalaryChange = this.handlefirstSalaryChange.bind(this);
   }
 
   handleApplicantsChange(event){
@@ -20,6 +27,9 @@ class MortgageForm extends Component {
     }
   }
 
+  handlefirstSalaryChange(event){
+    this.setState({criteria:{firstSalary: event.target.value}});
+  }
 
 
   render(){
@@ -32,7 +42,7 @@ class MortgageForm extends Component {
       <label htmlFor="2">2</label>
       <br/><br/>
 
-      <input type="number" placeholder="Enter First Salary:" min="0"/>
+      <input type="number" placeholder="Enter First Salary:" min="0" value={this.state.criteria.firstSalary} onChange={this.handlefirstSalaryChange}/>
       {this.secondApplicant()}
 
       <br/><br/>
@@ -40,6 +50,10 @@ class MortgageForm extends Component {
 
       <br/><br/>
       <input type="number" placeholder="Other Monthly Commitments" min="0"/>
+
+
+      <br/><br/>
+      <input type="submit" value="Calculate"/>
 
       </form>
     )
